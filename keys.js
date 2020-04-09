8e9e168a1082d8d5d2d4c712cac016bb60dd78c3
Brooke G <brooke.a.guarienti@gmail.com>
2020-04-09 15:29:31 -0500

    installed all npm packages. Wrote our switch commands

--- keys.js
+++ keys.js
@@ -1,8 +1,16 @@
-```js
 console.log('this is loaded');
 
 exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
-```
+  
+// exports.bandsInTown = {
+//   id: process.env.SPOTIFY_ID,
+//   secret: process.env.SPOTIFY_SECRET
+// };
+
+// exports.omdb = {
+//   id: process.env.SPOTIFY_ID,
+//   secret: process.env.SPOTIFY_SECRET
+// };
--- liri.js
+++ liri.js
@@ -4,5 +4,39 @@ require("dotenv").config();
 // importing and storing the keys.js file within the key variable
 var keys = require("./keys.js");
 
+// grab the axios package
+var axios = require("axios");
+
+// grab the moment package
+var moment = require("moment");
+
+//
+var fs = require("fs");
+
 // accessing spotify key and set to spotify variable
+var Spotify = require("node-spotify-api");
 var spotify = new Spotify(keys.spotify);
+
+
+var command = process.argv[2];
+
+var input = process.argv.slice(3).join(" ");
+console.log("command: " + command,"input: " + input);
+
+switch(command) {
+    case "concert-this":
+        concert();
+        break;
+    case "spotify-this-song":
+        music();
+        break;
+    case "do-what-it-says":
+        movie();
+        break;
+    default:
+        console.log("Please submit a valid request");
+}
+
+function concert(){
+    var concertUrl = `https://rest.bandsintown.com/artists/${input}/events?app_id=codingbootcamp`;
+}
